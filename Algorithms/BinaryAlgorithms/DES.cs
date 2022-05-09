c0c0bbfa3aa8aa48b25d8e0c1ee39f250e9585f9
Krystian <krystian.szmigielski2@gmail.com>
2022-05-09 19:23:09 +0200

    Added cutting input to 64 bit blocks

--- Algorithms/BinaryAlgorithms/DES.cs
+++ Algorithms/BinaryAlgorithms/DES.cs
@@ -9,6 +9,8 @@ namespace BSK1.Algorithms.BinaryAlgorithms
 {
     internal class DES : Algorithm
     {
+        private int DataSize { get; } = 56;
+
         public DES(AlgorithmsFormViewModel viewModel) : base(viewModel)
         {
         }
@@ -20,12 +22,66 @@ namespace BSK1.Algorithms.BinaryAlgorithms
 
         public override string Encrypt(string input)
         {
-            throw new NotImplementedException();
+            List<string> inputParts = DivideInput(input); // Divide input to 64 bit parts
+            List<string> encryptedParts = new List<string>();
+
+            foreach (string part in inputParts)
+            {
+                encryptedParts.Add(ExecuteAlgorithm(part)); // Execute DES algorithm on every part
+            }
+
+            string result = string.Join("", encryptedParts.ToArray()); // Connect every encrypted part and return as a result
+
+            return result;
+            //throw new NotImplementedException();
         }
 
         public override bool IsKeyValid(string key)
         {
-            throw new NotImplementedException();
+            return true;
+            //throw new NotImplementedException();
+        }
+
+        // Methods
+        private string ExecuteAlgorithm(string input)
+        {
+            return input;
+        }
+
+        private List<string> DivideInput(string input)
+        {
+            // one ascii char = 16 bits
+            List<string> parts = Split(input, DataSize).ToList(); // cut to 56 bits strings
+
+            for (int i = 0; i < parts.Count; i++)
+            {
+                parts[i] = AddMissingBytes(parts[i]);
+            }
+
+            return parts;
+        }
+
+        private IEnumerable<string> Split(string str, int chunkSize)
+        {
+            if (String.IsNullOrEmpty(str) || chunkSize < 1)
+                throw new ArgumentException();
+
+            for (int i = 0; i < str.Length; i += chunkSize)
+                yield return str.Substring(i, Math.Min(chunkSize, str.Length - i));
+        }
+
+        private string AddMissingBytes(string input)
+        {
+            int stringLength = input.Length;
+            int difference = DataSize - stringLength;
+            int bytesNumberToAdd = difference / 8;
+
+            for (int i = 0; i < bytesNumberToAdd; i++)
+                input += "00000000";
+
+            input += Convert.ToString(bytesNumberToAdd, 2).PadLeft(8, '0');
+
+            return input;
         }
     }
 }
